e2f76540cc86a8ce61a8cfb6781f733435b6453b
girish sharma <girish@sozeno.com>
2023-01-04 16:17:05 +0530

    update react hooks

--- src/App.js
+++ src/App.js
@@ -1,51 +1,47 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 import './App.css';
 import ImgList from './Components/ImgList';
 import SearchForm from './Components/SearchForm';
 
-export default class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			imgs: [],
-			loadingState: true
-		};
-	}
+const App = () => {
+	const [imgs,setImages] = useState([]);
+	const [loadingState,setLoadingState] = useState(false);
 
-	componentDidMount() {
-		this.performSearch();
-	}
-
-	performSearch = (query = 'god') => {
+	const performSearch = (query = 'god') => {
+		setLoadingState(true);
 		axios
 			.get(
 				`https://api.unsplash.com/search/photos/?page=1&per_page=15&query=${query}&client_id=P9H7xeXPeu5iZInKev8lSEQ34dKT4CwyEzKrwcoFf-A`
 			)
 			.then(data => {
-				this.setState({ imgs: data.data.results, loadingState: false });
+				setImages(data.data.results);
+				setLoadingState(false);
 			})
 			.catch(err => {
 				console.log('Error happened during fetching!', err);
 			});
 	};
 
-	render() {
-		return (
-			<div>
-				<div className="main-header">
-					<div className="inner">
-						<h1 className="main-title">Reach Photo Search </h1>
-						<SearchForm onSearch={this.performSearch} />
-					</div>
-				</div>
-				<div className="main-content">
-					{this.state.loadingState
-						? <p>Loading</p>
-						: <ImgList data={this.state.imgs} />}
-				</div>
-			</div>
-		);
-	}
+	useEffect(() => {
+		performSearch();
+	}, []);
+	
+
+	return <div>
+	<div className="main-header">
+		<div className="inner">
+			<h1 className="main-title">Reach Photo Search </h1>
+			<SearchForm onSearch={performSearch} />
+		</div>
+	</div>
+	<div className="main-content">
+		{loadingState
+			? <p>Loading</p>
+			: <ImgList data={imgs} />}
+	</div>
+</div>
 }
+
+export default App
--- src/Components/SearchForm.js
+++ src/Components/SearchForm.js
@@ -1,35 +1,67 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 
-export default class SearchForm extends Component {
-	state = {
-		searchText: ''
-	};
+// export default class SearchForm extends Component {
+// 	state = {
+// 		searchText: ''
+// 	};
 
-	onSearchChange = e => {
-		this.setState({ searchText: e.target.value });
-	};
+// 	onSearchChange = e => {
+// 		this.setState({ searchText: e.target.value });
+// 	};
 
-	handleSubmit = e => {
+// 	handleSubmit = e => {
+// 		e.preventDefault();
+// 		this.props.onSearch(this.query.value);
+// 		e.currentTarget.reset();
+// 	};
+
+// 	render() {
+// 		return (
+// 			<form className="search-form" onSubmit={this.handleSubmit}>
+// 				<label className="is-hidden" htmlFor="search">search</label>
+// 				<input
+// 					type="search"
+// 					onChange={this.onSearchChange}
+// 					ref={input => (this.query = input)}
+// 					name="search"
+// 					placeholder="Search Your Images"
+// 				/>
+// 				<button type="submit" id="submit" className="search-button">
+// 					<i className="material-icons icn-search">Search</i>
+// 				</button>
+// 			</form>
+// 		);
+// 	}
+// }
+
+
+const SearchForm = ({ onSearch }) => {
+	const [searchText, setSearchText] = useState();
+
+	const onSearchChange = (e) => {
+		setSearchText(e.target.value);
+	}
+
+	const handleSubmit = e => {
 		e.preventDefault();
-		this.props.onSearch(this.query.value);
+		onSearch(searchText);
 		e.currentTarget.reset();
 	};
 
-	render() {
-		return (
-			<form className="search-form" onSubmit={this.handleSubmit}>
-				<label className="is-hidden" htmlFor="search">search</label>
-				<input
-					type="search"
-					onChange={this.onSearchChange}
-					ref={input => (this.query = input)}
-					name="search"
-					placeholder="Search Your Images"
-				/>
-				<button type="submit" id="submit" className="search-button">
-					<i className="material-icons icn-search">Search</i>
-				</button>
-			</form>
-		);
-	}
+
+	return <form className="search-form" onSubmit={handleSubmit}>
+		<label className="is-hidden" htmlFor="search">search</label>
+		<input
+			type="search"
+			onChange={onSearchChange}
+			// ref={input => (this.query = input)}
+			name="search"
+			placeholder="Search Your Images"
+		/>
+		<button type="submit" id="submit" className="search-button">
+			<i className="material-icons icn-search">Search</i>
+		</button>
+	</form>
 }
+
+export default SearchForm;
